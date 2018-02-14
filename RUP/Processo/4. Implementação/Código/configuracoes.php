4b0299248baf80f05a48b87e1cf9baff3825ee9d
Guilherme Bornia <gborniam@gmail.com>
2018-02-14 19:47:21 -0200

    Atualização para o Boostrap v4

--- RUP/Processo/4. Implementação/Código/configuracoes.php
+++ RUP/Processo/4. Implementação/Código/configuracoes.php
@@ -1,67 +1,56 @@
-<!DOCTYPE html>
+<!doctype html>
+
 <html lang="pt-br">
   <head>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!--
-      * The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags
-      * A segunda tag é utilizar para criar uma compatibilidade com o Internet Explorer.
-      * A terceira tag é utiliza para criar designs responsivos.
-      ** O viewport é todo o conteúdo da página que é exibida pelo browser.
-      ** O width=device-width, initial-scale=1 ajuda o conteúdo da página conforme o tamanho do viewport.
-    -->
-    <title> WIP | Configurações </title>
-    
-    <!-- Bootstrap -->
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-    <!-- makes browsers render all elements more consistently and in line with modern standards. -->
-    <link href="style/normalize.css"        rel="stylesheet" type="text/css">
-    <link href="style/navbar.css"           rel="stylesheet" type="text/css">
-    <link href="style/configuracoes.css"    rel="stylesheet" type="text/css">
-    
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!--  -->
+    <link href="open-iconic/font/css/open-iconic-bootstrap.css" rel="stylesheet">
+    <!-- makes browsers render all elements more consistently and in line with modern standards -->
+    <link href="style/normalize.css" rel="stylesheet" type="text/css">
+    <!--  -->
+    <link href="style/navbar.css" rel="stylesheet" type="text/css">
+    <!--  -->
+    <link href="style/configuracoes.css" rel="stylesheet" type="text/css">
 
-    <!-- Substitui/adiciona algumas formatações do bootstrap -->
-    <style type="text/css" rel="stylesheet">
-      .lowweight {
-        font-weight: 100;
+    <title> Configurações • OurBills </title>
+
+    <style type="text/css">
+      .fonte-tamanho-notificacoes {
+        font-size: 0.85em;
       }
     </style>
   </head>
+
   <body>
-  	<header role="banner">
+    <header role="banner">
       <?php require("navbar-in.html"); ?> 
     </header>
    
-   	<div class="main container" role="main">
+    <div class="main container" role="main">
       <section id="configuracoes-pessoais" class="container-wip">
-        <div class="row">
-          <div class="col-md-11">
-            <h1> Configurações </h1>
-          </div>
+        <div class="row"> 
+          <h1> Configurações </h1>
         </div>
 
         <form method="" action="">
           <div class="row">
+            <!-- 
             <div class="col-md-2">
               <div class="row">
                 <div class="col-md-12" align="center">           
                   <img id="foto-perfil" src="img/no-profile-picture.jpg" class="img-responsive img-rounded" style="width: 10em; height: 10em;" alt="Foto de perfil">
                   <input id="btn-foto-perfil" type="file" class="btn btn-link pull-right" accept="image/png, image/jpeg"> Editar Foto </input>
                 </div>
-              </div> <!-- row - Foto de perfil -->
-            </div> <!-- col - Foto de perfil -->
+              </div> 
+            </div>
+             -->
 
-
-
-            <div class="col-md-4 col-md-offset-1">
+            <div class="col-md-5">
               <div class="row">
                 <div class="panel">
                   <h4> Dados Primários </h4>
@@ -71,7 +60,7 @@
               <div class="row">
                 <div class="col-md-12">
                   <div class="form-group">
-                    <label for="nome"> Nome </label>
+                    <label for="nome" class="font-weight-bold"> Nome </label>
                     <input id="nome" type="text" name="nome" class="form-control" value="<?= $nome = 'Maria Paula Oliveira' ?>" required>
                   </div>
                 </div>
@@ -80,7 +69,7 @@
               <div class="row">
                 <div class="col-md-12">
                   <div class="form-group">
-                    <label for="email"> E-mail </label>
+                    <label for="email" class="font-weight-bold"> E-mail </label>
                     <input id="email" type="email" name="email" class="form-control" value="<?= $email = 'maria@gmail.com' ?>" autocomplete="off" required>
                   </div>
                 </div>
@@ -89,7 +78,7 @@
               <div class="row">      
                 <div class="col-md-12">
                   <div class="form-group">
-                    <label for="pais_moeda"> País e Moeda </label>
+                    <label for="pais_moeda" class="font-weight-bold"> País e Moeda </label>
                     <?php require('paises_moedas.html'); ?>
                   </div>
                 </div>
@@ -98,15 +87,15 @@
               <div class="row">
                 <div class="col-md-12">
                   <div class="form-group">
-                    <label for="senha"> Senha </label>
-                    <button type="button" class="btn-link pull-right" aria-label="Editar senha"> Editar </button>
+                    <label for="senha" class="font-weight-bold"> Senha </label>
+                    <a class="btn btn-link btn-sm float-right" href="#" role="button"> Editar </a>
                     <input id="senha" type="password" name="senha" class="form-control" value="<?= $senha = '123456789' ?>" readonly>
                   </div>
                 </div>
               </div> <!-- Senha -->
             </div> <!-- Dados primários -->
 
-            <div class="col-md-4 col-md-offset-1">
+            <div class="col-md-5 offset-md-2">
               <div class="row">
                 <div class="panel">
                   <h4> Dados Secundários </h4>
@@ -116,14 +105,14 @@
               <div class="row">
                 <div class="col-md-6">
                   <div class="form-group">
-                    <label for="celular"> Celular </label>
+                    <label for="celular" class="font-weight-bold"> Celular </label>
                     <input id="celular" type="tel" name="celular" class="form-inline form-control" placeholder="DD+número" pattern="[0-9]{11}" title=" ** Não insira espaços, letras e caracteres especiais. &#013; ** O número de celular deve conter 11 dígitos. **" data-toggle="tooltip" data-placement="bottom">
                   </div>
                 </div> <!-- Celular -->
 
                 <div class="col-md-6">
                   <div class="form-group">
-                    <label for="nascimento"> Nascimento </label>
+                    <label for="nascimento" class="font-weight-bold"> Nascimento </label>
                     <input id="nascimento" type="date" name="nascimento" class="form-control" max="2018-01-01">
                   </div>
                 </div>
@@ -132,7 +121,7 @@
               <div class="row">
                 <div class="col-md-12">
                   <div class="form-group">
-                    <label for="estado"> Estado </label>
+                    <label for="estado" class="font-weight-bold"> Estado </label>
                     <select id="estado" name="estado" class="form-control">
                       <option value="saopaulo"> São Paulo </option>
                     </select>
@@ -142,55 +131,79 @@
 
               <div class="row">
                 <div class="panel">
-                  <h4> Notificações <small> Receber notificões quando: </small> </h4>
+                  <h4> Notificações <small class="text-muted"> Receber notificões quando: </small> </h4>
                 </div>
               </div>
 
               <div class="row">
                 <div class="col-md-12">
                   <div class="form-group">
-                    <div>
-                      <label class="lowweight">
-                        <input type="checkbox" name="notificacoes" value="grupo_criado_modificado">
-                        Um <u>grupo</u> for criado/modificado.
-                      </label>
+                    <div class="row">
+                      <div class="col">
+                        <label>
+                          <input type="checkbox" name="notificacoes" value="grupo_criado_modificado">
+                          <span class="fonte-tamanho-notificacoes"> Um <u>grupo</u> for criado/modificado. </span>
+                        </label>
+                      </div>
 
-                      <button class="btn btn-link btn-xs pull-right" data-toggle="tooltip" title="O usuário será notificado quando um grupo, no qual ele foi incluído, for criado, excluído ou sofrer qualquer tipo de modificação." data-placement="left">
-                        <span class="glyphicon glyphicon-info-sign"> </span>
-                      </button>
+                      <div class="col-1">
+                        <button type="button" class="btn btn-link btn-sm float-right" data-toggle="tooltip" data-placement="left" title="O usuário será notificado quando um grupo, no qual ele foi incluído, for criado, excluído ou sofrer qualquer tipo de modificação.">
+                          <small>
+                            <span class="oi oi-info align-top" title="Mais informações." aria-hidden="true"></span>
+                          </small>
+                        </button>
+                      </div>
                     </div>
                   
-                    <div>
-                      <label class="lowweight">
-                        <input type="checkbox" name="notificacoes" value="evento_incluido_alterado">
-                        Um <u>evento</u> for incluído/alterado.
-                      </label>
+                    <div class="row">
+                      <div class="col">
+                        <label>
+                          <input type="checkbox" name="notificacoes" value="evento_incluido_alterado">
+                          <span class="fonte-tamanho-notificacoes"> Um <u>evento</u> for incluído/alterado. </span>
+                        </label>
+                      </div>
 
-                      <button class="btn btn-link btn-xs pull-right" data-toggle="tooltip" title="O usuário será notificado quando um evento, no qual ele foi incluído, for adicionado, excluído ou sofrer qualquer tipo de alteração." data-placement="left">
-                        <span class="glyphicon glyphicon-info-sign"> </span>
-                      </button>
+                      <div class="col-1">
+                        <button type="button" class="btn btn-link btn-sm float-right" data-toggle="tooltip" data-placement="left" title="O usuário será notificado quando um evento, no qual ele foi incluído, for adicionado, excluído ou sofrer qualquer tipo de alteração.">
+                          <small>
+                            <span class="oi oi-info align-top" title="Mais informações." aria-hidden="true"></span>
+                          </small>
+                        </button>
+                      </div>
                     </div>
 
-                    <div>
-                      <label class="lowweight">
-                        <input type="checkbox" name="notificacoes" value="conta_expirar">
-                        Faltar 2 dias para uma conta expirar.
-                      </label>
+                    <div class="row">
+                      <div class="col">
+                        <label>
+                          <input type="checkbox" name="notificacoes" value="conta_expirar">
+                          <span class="fonte-tamanho-notificacoes"> Faltar 2 dias para uma conta expirar. </span>
+                        </label>
+                      </div>
 
-                      <button class="btn btn-link btn-xs pull-right" data-toggle="tooltip" title="O usuário será notificado 2 dias antes de uma conta, na qual ele foi incluído, atingir sua data de pagamento." data-placement="left">
-                        <span class="glyphicon glyphicon-info-sign"> </span>
-                      </button>
+                      <div class="col-1">
+                        <button type="button" class="btn btn-link btn-sm float-right" data-toggle="tooltip" data-placement="left" title="O usuário será notificado 2 dias antes de uma conta, na qual ele foi incluído, atingir sua data de pagamento.">
+                          <small>
+                            <span class="oi oi-info align-top" title="Mais informações." aria-hidden="true"></span>
+                          </small>
+                        </button>
+                      </div>
                     </div>
 
-                    <div>
-                      <label class="lowweight">
-                        <input type="checkbox" name="notificacoes" value="novas_funcionalidades">
-                        Surgirem novas funcionalidades.
-                      </label>
+                    <div class="row">
+                      <div class="col">
+                        <label>
+                          <input type="checkbox" name="notificacoes" value="novas_funcionalidades">
+                          <span class="fonte-tamanho-notificacoes"> Surgirem novas funcionalidades. </span>
+                        </label>
+                      </div>
 
-                      <button class="btn btn-link btn-xs pull-right" data-toggle="tooltip" title="O usuário será notificado quando novas funcionalidades ficarem disponíveis." data-placement="left">
-                        <span class="glyphicon glyphicon-info-sign"> </span>
-                      </button>
+                      <div class="col-1">
+                        <button type="button" class="btn btn-link btn-sm float-right" data-toggle="tooltip" data-placement="left" title="O usuário será notificado quando novas funcionalidades ficarem disponíveis.">
+                          <small>
+                            <span class="oi oi-info align-top" title="Mais informações." aria-hidden="true"></span>
+                          </small>
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -209,12 +222,14 @@
       </section> <!-- Configurações Pessoais -->
     </div>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="bootstrap/js/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-    <!-- Meus Eventos JavaScript -->
-    <script src="js/configuracoes.js" type="text/javascript"> </script>
+    <!-- Personal -->
+    <!--  -->
+    <script type="text/javascript" src="js/configuracoes.js"> </script>
   </body>
 </html>
