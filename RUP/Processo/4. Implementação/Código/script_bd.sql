090dda314fd0a57157a4ba7d47451f0086680817
Guilherme Bornia <gborniam@gmail.com>
2018-03-25 00:31:35 -0300

    Banco de dados atualiaado com relação entre usuários e grupos

--- RUP/Processo/4. Implementação/Código/script_bd.sql
+++ RUP/Processo/4. Implementação/Código/script_bd.sql
@@ -3,9 +3,9 @@ CREATE DATABASE wip;
 USE wip;
 
 CREATE TABLE usr (
-	usr_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	usuario_id INT NOT NULL AUTO_INCREMENT,
 	nome VARCHAR(50) NOT NULL,
-	email VARCHAR(100) NOT NULL, -- Serve para o usuário fazer login
+	email VARCHAR(100) NOT NULL UNIQUE, -- Serve para o usuário fazer login
 	senha VARCHAR(32) NOT NULL,
 	pais_moeda VARCHAR(30) NOT NULL,
 	celular CHAR(11) NULL,
@@ -18,5 +18,32 @@ CREATE TABLE usr (
 		Ligado 3/-3 Desligado ------ Faltar 2 dias para uma conta expirar
 		Ligado 4/-4 Desligado ------ Novas funcionalidades forem lançadas
 	*/
-	conheceu_ferramenta VARCHAR(10) NOT NULL
+	conheceu_ferramenta VARCHAR(10) NOT NULL,
+
+	PRIMARY KEY(usr_id),
+);
+
+CREATE TABLE grupos (
+	grupo_id INT NOT NULL AUTO_INCREMENT,
+	titulo VARCHAR(30) NOT NULL,
+	descricao VARCHAR(100) NULL,
+	ultima_att DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+	PRIMARY KEY(grupo_id)
+);
+
+CREATE TABLE usuario_pertence_grupo (
+	grupos_membros_id INT NOT NULL AUTO_INCREMENT,
+	grupo_id_ref INT NOT NULL,
+	usuario_id_ref INT NOT NULL,
+	permissao TINYINT NOT NULL,
+	/*
+		1 ------ Pode ver
+		2 ------ Pode editar
+		3 ------ É dono
+	*/
+
+	PRIMARY KEY(grupos_membros_id),
+	FOREIGN KEY(grupo_id_ref) REFERENCES grupos(grupo_id),
+	FOREIGN KEY (usuario_id_ref) REFERENCES usuarios(usuario_id)
 );
