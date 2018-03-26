edeff51205e9afc941bab8f565ab4697d04acd2d
Guilherme Bornia <gborniam@gmail.com>
2018-03-25 21:21:10 -0300

    Apenas alguns comentários que foram retirados do código

--- RUP/Processo/4. Implementação/Código/reqs/deletar_grupos.php
+++ RUP/Processo/4. Implementação/Código/reqs/deletar_grupos.php
@@ -9,20 +9,6 @@ $con = (new db())->conecta_mysql();
 
 $grupos = implode(",", $_POST['grupos']);
 $sessao_id = $_SESSION['id'];
-/*
-// Verificar se o usuário tem permissão para excluir o grupo
-$sql = "SELECT grupos_membros_id, permissao FROM usuario_pertence_grupo WHERE usuario_id_ref = $sessao_id AND grupo_id_ref IN ($grupos);";
-
-// Executa a query
-$res = mysqli_query($con, $sql);
-
-$grupos_permitidos = array();
-while ($data = mysqli_fetch_array($res, MYSQLI_ASSOC)) {
-	if($data['permissao'] == 3) {
-
-	}
-}
-*/
 
 $sql = "DELETE FROM usuario_pertence_grupo WHERE grupo_id_ref IN ($grupos);";
 
